479ab7591d8345e74eda100adf956f90380995b4
Savidude <mysticsavy@gmail.com>
2020-01-16 17:04:12 +0530

    Copy to GOROOT with action

--- .github/actions/copy-gopath/index.js
+++ .github/actions/copy-gopath/index.js
@@ -1,2 +1,16 @@
-const gopath = process.env.$GOROOT;
-console.log("$GOROOT: " + gopath);
+let fx = require('mkdir-recursive');
+const copydir = require('copy-dir');
+
+const goroot = process.env.GOROOT;
+const workspaceDir = process.env.GITHUB_WORKSPACE;
+
+let projectSource = goroot + "/src/github.com/wso2";
+fx.mkdir(projectSource, function (err) {
+    console.log("Created project source " + projectSource)
+});
+
+copydir.sync(workspaceDir, projectSource, {
+    utimes: true,
+    mode: true,
+    cover: true
+});
